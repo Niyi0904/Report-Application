f9c8063b85726364edd2d7142d248aeae9f69eb8
YourUsername <YourEmail@example.com>
2025-01-17 03:19:38 +0100

    Building responsiveness

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -2,6 +2,8 @@ import './navbar.component.css';
 import { CgProfile } from "react-icons/cg";
 import { IoSettingsOutline } from "react-icons/io5";
 import { FaHome } from "react-icons/fa";
+import { Routes, Route, Navigate } from "react-router-dom";
+
 
 import { auth } from '../../firebase/firebase.utils';
 
@@ -9,15 +11,20 @@ import { UseStateContext } from '../../context/contextProvider';
 
 
 const Navbar = () => {
-    const consoles = () => console.log('niyi');
-    const {currentUserProfile} = UseStateContext(); 
+    const {currentUserProfile, setCurrentUserprofile} = UseStateContext(); 
+
+    const signOut = () => {
+        alert('Warning!! You are about to sign out')
+        auth.signOut();
+        setCurrentUserprofile(null);
+    }
     return (
         <div className='mt-2 px-2 w-full border-b-2 border-[#7E6227]'>
             <div className='flex justify-between'>
                 <div className='navbar-header'>
                     Welcome {currentUserProfile.name}
                 </div>
-                <div className='flex navbar-icons space-x-11 xs:space-x-5 sm:space-x-5 top-2 relative w-[15%] xs:w-[25%] sm:w-[25%] text-end'>
+                <div className='flex navbar-icons space-x-11 xs:space-x-5 sm:space-x-5 top-2 relative w-[15%] xs:w-[30%] sm:w-[25%] text-end'>
                     <button onClick={auth.signOut} className='relative bottom-1'>
                         <FaHome />
                     </button>
@@ -26,7 +33,7 @@ const Navbar = () => {
                         <IoSettingsOutline />
                     </button>
                     
-                    <button onClick={auth.signOut} className='relative bottom-1'>
+                    <button onClick={() => signOut() } className='relative bottom-1'>
                         <CgProfile />
                     </button>
                 </div>
