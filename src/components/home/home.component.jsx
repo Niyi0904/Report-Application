cba59c8fdf91d4347d7cdc9b94e16cd0c8cc5d3f
YourUsername <YourEmail@example.com>
2025-01-24 02:05:22 +0100

    testing

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -12,7 +12,7 @@ const Home = ({ id, data }) => {
     <div className='home-container'>
         <div className='relative bg-white w-[85%] min-h-96 mx-auto top-12 rounded-md'>
             <div className='text-center'>
-                <div className='relative grid grid-cols-3 gap-y-6 gap-x-6 mx-auto px-3 text-left w-[100%] top-2'>
+                <div className='relative grid grid-cols-3 gap-y-4 gap-x-6 mx-auto px-3 text-left w-[100%] top-2'>
                     <Link to='/evangelismPage'>  
                         <button className='home-btn'>Evangelism Reports</button>
                     </Link>
